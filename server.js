3ae6f31700b333df4eb021423a675f380553230b
Pradeep <webreinvent@gmail.com>
2021-06-17 06:36:49 +0530

    server

--- server.js
+++ server.js
@@ -63,5 +63,5 @@ app.listen(80, (err, address) => {
         app.log.error(err)
         process.exit(1)
     }
-    app.log.info(`server listening on ${address}`)
+    //app.log.info(`server listening on ${address}`)
 })
